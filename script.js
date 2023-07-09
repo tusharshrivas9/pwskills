6d1cd58987eeacb8abc991e18ccc1a80cbf0c0a0
tusharshrivas9 <122074190+tusharshrivas9@users.noreply.github.com>
2023-07-09 19:57:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var box = document.getElementById('box');
+  
+    box.addEventListener('click', function() {
+      box.style.backgroundColor = 'yellow';
+      box.style.width = '600px';
+      box.style.height = '600px';
+    });
+  });
+  
